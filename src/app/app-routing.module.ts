9198627e2f3549db96d45764aac95cfef90e700f
sirine sabbeh <sabbehsyrine@gmail.com>
2022-06-16 22:02:11 +0200

    MyPersonalWebSite

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './profile/about/about.component';
+import { EducationComponent } from './profile/education/education.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+{path:'',component:AboutComponent},
+{path:'Education',component:EducationComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ActivatedRouteSnapshot } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './profile/header/header.component';
+import { AboutComponent } from './profile/about/about.component';
+import { NavbarComponent } from './profile/navbar/navbar.component';
+import { EducationComponent } from './profile/education/education.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    NavbarComponent,
+    EducationComponent 
+],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
